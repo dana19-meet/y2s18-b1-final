390d0ab2bbcb4f8c63154a331b0f5fccbb51ab9c
aboud20-meet <aboud20@meet.mit.edu>
2018-08-13 11:43:17 +0300

    h

--- app.py
+++ app.py
@@ -3,35 +3,52 @@ from flask import Flask, render_template, url_for, redirect, request, session
 
 # Add functions you need from databases.py to the next line!
 from database import *
-
+import datetime
 # Starting the flask app
 app = Flask(__name__)
 
 # App routing code here
 @app.route('/')
 def home():
-    return render_template('home.html',donates=query_all())
+    return render_template('home.html',donates=query_all_donates())
 
-@app.route('/signup_donor')
+@app.route('/signup_donor', methods=['GET', 'POST'])
 def donor():
+	print(request.method)
 	if request.method=='POST':
+		print("here")
 		add_donor(request.form['donor_name'],
 			request.form['email'],
 			request.form['password'],
 			request.form['area'],
 			request.form['address'],
 			request.form['telephone'])
-		return render_template('feed_for_recievers.html')
+		return render_template('feed_for_recievers.html', donates=query_all_donates())
 	if request.method=='GET':
 		return render_template('signup_donor.html')
 
-@app.route('/signup_reciever')
+@app.route('/signup_reciever', methods=['GET', 'POST'])
 def reciever():
-	return render_template('signup_reciever.html')
+	if request.method=='POST':
+		add_reciever(request.form['reciever_name'],
+			request.form['email'],
+			request.form['password'],
+			request.form['area'],
+			request.form['telephone'])
+		return render_template('feed_for_recievers.html', donates=query_all_donates())
+	if request.method=='GET':
+		return render_template('signup_reciever.html')
 
-@app.route('/feed')
+@app.route('/feed', methods=['GET', 'POST'])
 def feed():
-	return render_template('feed_for_recievers.html')
+	if request.method=='POST':
+		exp=datetime.datetime.strptime(request.form['expiration_date'],'%Y-%m-%d').date()
+		add_donation(request.form['donation_name'],
+			request.form['amount'],
+			exp)
+		return render_template('feed_for_recievers.html', donates=query_all_donates())
+	if request.method=='GET':
+		return render_template('feed_for_recievers.html', donates=query_all_donates())
 # @app.route('/donation/<int:student_id>')
 # def display_student(student_id):
 #     return render_template('student.html', student_id=student_id)
--- database.py
+++ database.py
@@ -16,15 +16,25 @@ def add_donation(name, amount, expiration_date):
 	session.add(donation_object)
 	session.commit()
 
-def add_donor(name, email, password, area, adress, phone_number):
+def add_donor(name, email, password, area, address, telephone):
 	donor_object = Donor(
 		name=name,
 		email = email,
 		password = password,
 		area = area,
-		adress=adress,
-		phone_number=phone_number)
+		address=address,
+		telephone=telephone)
 	session.add(donor_object)
+	session.commit()
+
+def add_reciever(reciever_name, email, password, area, telephone):
+	reciever_object = Reciever(
+		reciever_name=reciever_name,
+		email = email,
+		password = password,
+		area = area,
+		telephone=telephone)
+	session.add(reciever_object)
 	session.commit()
 
 def delete_donation(donation_id):
@@ -32,9 +42,13 @@ def delete_donation(donation_id):
 		donation_id=donation_id).delete()
 	session.commit()
 
-def query_all():
+def query_all_donates():
 	donations = session.query(Donation).all()
 	return donations
+
+def query_all_donors():
+	donors = session.query(Donor).all()
+	return donors
 
 def query_by_id(donation_id):
     donation = session.query(Donation).filter_by(
